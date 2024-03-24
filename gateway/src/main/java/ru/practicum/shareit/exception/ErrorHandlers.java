021a013bea7947cd865476bd74f065244ce85ab8
OrionSleeve <akbaroff.t@yandex.ru>
2024-03-24 12:53:22 +0700

    sprint 16 tests

--- gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandlers.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandlers.java
@@ -1,42 +1,39 @@
 package ru.practicum.shareit.exception;
 
+import lombok.Builder;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpStatusCodeException;
+
+import javax.validation.ConstraintViolationException;
 
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandlers {
-    @ExceptionHandler({MethodArgumentNotValidException.class, ValidationException.class})
+
+    @ExceptionHandler({ConstraintViolationException.class, HttpStatusCodeException.class,
+            InvalidRequestException.class, BindException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleValidationExceptions(RuntimeException e) {
-        log.error("Validation Error: {}", e.getMessage(), e);
-        return new ErrorResponse("validation error: " + e.getMessage());
-    }
-
-    @ExceptionHandler(NotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse notFoundError(NotFoundException e) {
-        log.error("Not Found Error: {}", e.getMessage(), e);
-        return new ErrorResponse(String.format("not found: %s", e.getMessage()));
-    }
-
-    @ExceptionHandler(Throwable.class)
-    public ResponseEntity<ErrorResponse> handleThrowable(Throwable ex) {
-        log.error("An unexpected error occurred: {}", ex.getMessage(), ex);
-        ErrorResponse errorResponse = new ErrorResponse("internal server error: " + ex.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    public ErrorResponse handlerBadRequest(final RuntimeException exception) {
+        log.debug("Получен статус 404 Bad request {}", exception.getMessage(), exception);
+        return ErrorResponse.builder().error(exception.getMessage()).build();
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleStateException(final InvalidRequestException ex) {
-        log.error("An unexpected error occurred: {}", ex.getMessage(), ex);
-        return new ErrorResponse(ex.getMessage());
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handlerInternalServerError(final Throwable exception) {
+        log.debug("Получен статус 500 Internal server error {}", exception.getMessage(), exception);
+        return ErrorResponse.builder().error(exception.getMessage()).build();
     }
 
+    @Data
+    @Builder
+    public static class ErrorResponse {
+        private final String error;
+    }
 }
